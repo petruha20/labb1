8004a156e3270fce6a62ee2fbd8021d7be6a0056
edu-msk22-804s <edu-msk22-804s@MSK22-EWS80416>
2025-09-23 10:25:32 +0300

    Добавьте файлы проекта.

---
+++ labb1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyCSharpProject.Models; // Подключаем наше пространство имён
+
+Console.WriteLine("Привет! Это моя первая программа на C#!");
+
+// Создаем объект человека
+Person person = new Person { Name = "Алексей", Age = 25 };
+
+// Выводим информацию о нем
+Console.WriteLine($"Создан человек: {person.Name}, возраст: {person.Age}");
+
+Console.WriteLine("Программа завершена. Нажмите любую клавишу...");
+Console.ReadKey(); // Чтобы консоль не закрылась сразу
---
+++ labb1/Новая папка/models/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MyCSharpProject.Models;
+
+public class Person
+{
+    public string Name { get; set; } = "Неизвестно";
+    public int Age { get; set; }
+}
